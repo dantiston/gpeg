03aa1cde267a6d7b587ac66f6d86f4863f735bba
T.J. Trimble <trimblet@me.com>
2023-09-26 22:01:01 -0700

    basic functionality working

--- src/gpeg/cli.py
+++ src/gpeg/cli.py
@@ -2,10 +2,23 @@
 
 import argparse
 import sys
+import traceback
+
+import pe
 
 
 def main(args: argparse.Namespace) -> int:
-    return 0
+    if args.input is None:
+        args.input = sys.stdin.readlines()
+    try:
+        grammar = pe.compile(args.expression)
+        for line in args.input:
+            if grammar.match(line, flags=pe.MEMOIZE):
+                print(line.strip())
+        return 0
+    except Exception as e:
+        traceback.print_exception(e)
+        return 1
 
 
 def main_parser() -> argparse.ArgumentParser:
@@ -13,10 +26,11 @@ def main_parser() -> argparse.ArgumentParser:
         "gpeg",
         description="Search with Parsing Expressions: global/parsing expressions/print",
     )
-    parser.add_argument("grammar", help="The grammar to match")
+    parser.add_argument("expression", help="The expression to match")
     parser.add_argument(
         "input",
-        help="New line separated values to check against the grammar, defaults to stdin",
-        default=sys.stdin,
+        help="New line separated values to check against the expression, defaults to stdin",
+        type=argparse.FileType("r"),
+        default=None,
     )
     return parser
